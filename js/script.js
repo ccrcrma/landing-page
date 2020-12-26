faffe436c31fd60b5cb3d39c9914ef0d68bbff2b
ccrcrma <crmaccr96@gmail.com>
2020-12-26 09:32:10 +0545

    form and js fun

--- js/script.js
+++ js/script.js
@@ -2,11 +2,22 @@
 var navbar = document.getElementById("navbar")
 window.addEventListener("load", function(event){
     document.onscroll = function(event){
-        if(document.body.scrollTop >=50  || document.documentElement.scrollTop >=50){
+        if(document.body.scrollTop >=50  || document.documentElement.scrollTop >=50)
+        {
             navbar.classList.add("colored-navbar");
-        }else {
+        }
+        else 
+        {
             navbar.classList.remove("colored-navbar");
         }
     }
+
+    Object.values(document.querySelectorAll(".form-control")).forEach(function(elem){
+        elem.onfocus = function(){
+            console.log(elem.previousElementSibling.firstChild)
+            elem.previousElementSibling.firstChild.classList.add("blue-icon");
+        }
+    });
+
 });
-    
+  
